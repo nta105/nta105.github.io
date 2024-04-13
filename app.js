9cd5792ee8452230ea84e0f1c8cc84192f89db0c
nta105 <nguyenthienan96@gmail.com>
2024-04-13 13:08:58 -0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDE18rHHX0vxRPKoMCPyATBIJuj7r5LkGE",
   authDomain: "website-2906b.firebaseapp.com",
@@ -18,12 +15,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const db = firebase.firestore();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
@@ -35,11 +31,10 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
     })
     .then(() => {
         alert("Thank you! Your message has been sent.");
-        document.getElementById('bookingForm').reset(); // Reset form after submission
+        document.getElementById('contactForm').reset(); // Correct the form ID for resetting
     })
     .catch((error) => {
         console.error("Error adding document: ", error);
         alert("Sorry, there was a problem sending your message.");
     });
 });
-
